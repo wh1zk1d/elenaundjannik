32450f460c124ae068488eebb1d19cce4c0db48f
Jannik Baranczyk <jb@connect2.de>
2021-02-14 20:06:08 +0100

    add IE alert

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Switch, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { isIE } from 'react-device-detect'
 
 import Layout from './components/Layout'
 import Home from './views/Home'
@@ -9,6 +10,30 @@ import BetterCallHannah from './views/Hannah'
 
 const App = () => {
   const queryClient = new QueryClient()
+
+  if (isIE) {
+    return (
+      <div style={{ display: 'grid', placeItems: 'center', minHeight: '100vh', textAlign: 'center' }}>
+        <div>
+          <img
+            src='https://media.giphy.com/media/vPN3zK9dNL236/giphy.gif'
+            alt='Nope GIF'
+            style={{ maxWidth: '100%' }}
+          />
+          <h1
+            style={{
+              fontFamily: 'sans-serif',
+              lineHeight: '1.2',
+              maxWidth: '768px',
+              margin: '40px auto 0',
+            }}>
+            Sorry, aber mitm Internet Explorer funktioniert das hier nicht. Bitte benutz einen aktuelleren Browser, wie
+            Chrome, Firefox oder Edge.
+          </h1>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <QueryClientProvider client={queryClient}>
